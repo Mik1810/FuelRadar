96ad60f36d1ae01a38d1acf911f00077c41d7840
Michael Piccirilli <michaelpiccirilli3@gmail.com>
2026-07-19 23:41:02 +0200

    Remove serverExternalPackages to bundle postgres in same module scope

--- src/config/next-config.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "bun:test";
-
-import nextConfig from "../../next.config";
-
-test("keeps the PostgreSQL driver external to production server chunks", () => {
-  expect(nextConfig).toMatchObject({
-    serverExternalPackages: expect.arrayContaining(["postgres"]),
-  });
-});
